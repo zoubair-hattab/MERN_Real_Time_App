7568a32e68e744b94a62e1531370bb6e01876849
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-06-22 18:48:09 +0100

    add post  api

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,81 @@
+import Post from '../modeles/post.model.js';
+import ErrorHandler from '../utils/errorHnadler.js';
+export const createPost = async (req, res, next) => {
+  const { text, img } = req.body;
+  if (!text) {
+    return next(new ErrorHandler('the text is required.', 404));
+  }
+  const maxLenght = 500;
+  if (text.length > maxLenght)
+    return next(new ErrorHandler(`Text must be less then ${maxLenght}`, 404));
+  try {
+    const newPost = new Post({
+      postedBy: req.user,
+      text,
+      img,
+    });
+    const post = await newPost.save();
+    res.status(200).json({
+      success: true,
+      message: newPost,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+export const getPost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return next(new ErrorHandler('Post not found', 404));
+    }
+    res.status(200).json({
+      success: true,
+      message: post,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+export const deletePost = async (req, res, next) => {
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('The post has been deleted');
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+export const likeUnlikePost = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const currentUserId = req.user;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return next(new ErrorHandler('Post not found', 404));
+    }
+    userLike = post.likes.includes(currentUserId);
+    if (userLike) {
+      await Post.findByIdAndUpdate(postId, {
+        $pull: {
+          likes: currentUserId,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        message: 'Post Unlike successfully',
+      });
+    } else {
+      await Post.findByIdAndUpdate(postId, {
+        $push: {
+          likes: currentUserId,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        message: 'Post liked successfully',
+      });
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,3 +1,7 @@
 import express from 'express';
+import { isAuth } from '../midellware/isAuth.js';
+import { createPost } from '../controllers/post.controller.js';
 const router = express.Router();
+
+router.post('/create', isAuth, createPost);
 export default router;
